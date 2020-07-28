3a18c5415a4ee11ab8ebcd3ea359a81162950b7b
Vidhan Shah <shahvidhan123@gmail.com>
2020-07-28 12:35:05 +0530

    Form layout completed

---
+++ js/script.js
@@ -0,0 +1,63 @@
+function validate() {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const number = document.getElementById("number");
+  const message = document.getElementById("message");
+  
+  const blankErr = document.getElementById("blankErr");
+  const nameErr = document.getElementById("nameErr");
+  const emailErr = document.getElementById("emailErr");
+  const numberErr = document.getElementById("numberErr");
+
+  let b, n, e, num;
+
+  const nameCheck = /^[a-zA-Z ]+$/;
+  const emailCheck = /^([a-zA-Z0-9._-]+)@([a-zA-Z0-9.-]+).([a-zA-Z]+)(.[a-zA-Z]{2,8})?$/;
+  const numberCheck = /^\+?([0-9 ]{3})?([0-9- .]{10,13})$/;
+
+  if (name.value.trim()=="") {
+    n = 1;
+  }
+  else if (nameCheck.test(name.value)) {
+    n = 1;
+  }
+  else {
+    nameErr.style.visibility="visible";
+    n = 0;
+  };
+
+  if (email.value.trim()=="") {
+    blankErr.style.visibility="visible";
+    b = 0;
+  }
+  else if (emailCheck.test(email.value)) {
+    e = 1;
+  }
+  else {
+    emailErr.style.visibility="visible";
+    e = 0;
+  };
+
+  if (number.value.trim()=="") {
+    num = 1
+  }
+  else if (numberCheck.test(number.value)) {
+    num = 1;
+  }
+  else {
+    numberErr.style.visibility="visible";
+    num = 0;
+  };
+
+  if (message.value.trim()=="") {
+    blankErr.style.visibility="visible";
+    b = 0;   
+  };
+
+  if (b == 0 || n == 0 || e == 0 || num == 0) {
+    return false
+  }
+  else {
+    return true
+  };
+}
